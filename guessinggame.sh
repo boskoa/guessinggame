821bf7c02de5222343732d48743dc79375cef262
boskoa <bosko.ap@gmail.com>
2020-08-30 14:20:00 +0200

    first commit

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,5 +1,7 @@
+#!/usr/bin/env bash
+# File: guessinggame.sh
+
 num_of_files=$(ls | wc -l)
-echo $num_of_files
 answer=-1
 
 while [[ $answer -ne $num_of_files ]]
@@ -10,10 +12,14 @@ do
 	if [[ $answer -gt $num_of_files ]]
 	then
 		echo "Too high."
+
 	elif [[ $answer -lt $num_of_files ]]
 	then
 		echo "Too low."
+
 	else
 		echo "Congratulations!"
+	
 	fi
+
 done
